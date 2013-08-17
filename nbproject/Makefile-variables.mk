4c02f64c083a85bc8d933d16ccd813a2bc06ce2c
St. Artaxerxes <evolian.x64@aol.com>
2013-08-17 14:02:46 +0000

    Initialization of repository

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=xerxes
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/xerxes
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=xerxes.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/xerxes.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=xerxes
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/xerxes
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=xerxes.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/xerxes.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
